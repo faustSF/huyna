4ad90901bac91942eada272679a787a8c4f38bc7
faustSF <145801651+faustSF@users.noreply.github.com>
2023-12-01 01:19:17 +0300

    h

--- grafik/Source.cpp
+++ grafik/Source.cpp
@@ -7,8 +7,8 @@
 #include <cmath>
 using namespace sf;
 using namespace ImGui;
-const int W = 800;
-const int H = 600;
+const int W = 602;
+const int H = 602;
 /*#define W 800
 #define H 600
 using namespace sf;
@@ -29,31 +29,36 @@ void print(int& p1, int& i, float& c, int& scal,int& x0, int& y0, RenderWindow* 
 
 int main()
 {
-	const float z{1.02f};
+	const float z{1.1f};
 	float q1, l1, q2, l2;
 	q1 = 1;
-	l1 = 1;
+	l1 = 0;
 	q2 = 1;
 	l2=0;
 	float& a = q1;
 	float& b = l1;
 	float& k = q2;
 	float& d = l2;
-	int x0 = (W / 2)-100;
+	int x0 = (W / 2);
 	int y0 = (H / 2);
-	RenderWindow window(sf::VideoMode(800, 600), "Grafik");
+	RenderWindow window(sf::VideoMode(W, H), "Grafik");
 	SFML::Init(window);
 	int o1 = -10;
 	int o2 = 15;
 	float c = 100;
-	int mass = (((o1) * (-1) + o2) * 1000);
-	int sc = 45;
+	int mass = (((o1) * (-1) + o2)*c + 1);
+	int sc = 33;
 	bool circleExists = false;
+	bool background=true;
 	float circleRadius = 1.5f;
+	Texture t;
+	t.loadFromFile("D:/Grafik/Coordinate plane.png");
+	Sprite pic(t);
 	//int j = 3;
 	//int circleSegments = 100;
 	//float circleColor[3] = { (float)204 / 255, (float)77 / 255, (float)5 / 255 };
-	CircleShape point(circleRadius);
+	CircleShape Lpoint(circleRadius);
+	CircleShape& point = Lpoint;
 	point.setFillColor(Color::Green);
 	//point.setFillColor(sf::Color
 	//(
@@ -64,13 +69,14 @@ int main()
 	//shape.setOrigin(circleRadius, circleRadius);
 	//shape.setPosition(400, 400); // Center circle
 	
-	RectangleShape* OsX = new RectangleShape(Vector2f(W, 1));
-	OsX->setFillColor(Color::Blue);
-	OsX->setPosition(0,x0);
+	RectangleShape* OsX = new RectangleShape(Vector2f(W*2, 1*(sc/33)));
+	OsX->setFillColor(Color::Black);
+	OsX->setPosition(y0*(-1),x0);
 
-	RectangleShape* OsY = new RectangleShape(Vector2f(1, H));
-	OsY->setFillColor(Color::Blue);
-	OsY->setPosition(y0+95, 0);
+	RectangleShape* OsY = new RectangleShape(Vector2f(1 * (sc / 33), H*2));
+
+	OsY->setFillColor(Color::Black);
+	OsY->setPosition(y0, (-1)*x0);
 	int anim = 0;
 	View zoom(Vector2f(0,0), Vector2f(W, H));
 	zoom.move(Vector2f(W / 2, H / 2));
@@ -95,7 +101,7 @@ int main()
 				}
 				if (event.mouseWheelScroll.delta <= -1)
 				{
-					zoom.zoom(1.02f);
+					zoom.zoom(1.1f);
 				}
 				break;
 			
@@ -107,6 +113,7 @@ int main()
 		Begin("Parametrs");
 		ImGui::Text("Input in this shit");
 		Checkbox("just fo fun", &circleExists);
+		Checkbox("back", &background);
 		InputFloat("A", &a);
 		InputFloat("B", &b);
 		InputFloat("K", &k);
@@ -136,19 +143,30 @@ int main()
 
 			if (i < 20) {
 				if (i < 10)
-					line[i].setPosition(x0+95 - (i + 1) * sc, y0 - 10);
+					line[i].setPosition(x0 - (i + 1) * (sc), y0 - 10);
 				else
-					line[i].setPosition(x0+95 + (i -9) * sc, y0 - 10);
+					line[i].setPosition(x0 + (i -9) * (sc), y0 - 10);
 			}
 			else {
 				line[i].setRotation(90);
 				if (i < 30)
-					line[i].setPosition(x0 + 105, y0 + (i - 30) * (-1)*sc);
+					line[i].setPosition(x0+10 , y0 + (i - 30) * (-1)*sc);
 				else
-					line[i].setPosition(x0 + 105, y0 + (i - 29) * (-1)*sc);
+					line[i].setPosition(x0+10 , y0 + (i - 29) * (-1)*sc);
 			}
 		}
-		window.clear(sf::Color(18, 33, 43)); // Color background
+		window.clear(sf::Color::White);
+		if (background)
+			window.draw(pic);
+		else
+		{
+			//window.draw(*OsX);
+			//window.draw(*OsY);
+			for (int i = 0; i < 40; i++)
+				if (i != 19 && i != 20)
+					window.draw(line[i]);
+		}
+
 		if (circleExists)
 			for (int i = 0; i < anim; i++) {
 				float x = o1 + i / c;
@@ -157,18 +175,20 @@ int main()
 			
 
 				float x1 = x0 + x * (sc);
-				float y1 = y0 - y * (sc)+5;
+				float y1 = y0 - y * (sc);
 
 				point.setPosition(x1, y1);
 				window.draw(point);
 			}
 		    
-	        window.draw(*OsX);
+	        /*window.draw(*OsX);
 		    window.draw(*OsY);
 			for (int i = 0; i < 40; i++)
 				if (i != 19 && i != 20)
-					window.draw(line[i]);
-			window.setView(zoom);
+					window.draw(line[i]);*/
+		    window.draw(*OsX);
+		    window.draw(*OsY);
+		    window.setView(zoom);
 		    SFML::Render(window);
 		    window.display();
 	}
